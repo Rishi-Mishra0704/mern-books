edaa0bd919fc6d2816fdf0366cfe8acb1d122821
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-08 02:56:08 +0530

    Finetune the code

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { PORT, DB_URL } from "./config.js";
-import { Book } from "./models/BookModel.js";
+import { PORT, DB_URL } from "./config.js"; // Import configuration variables
+import { Book } from "./models/BookModel.js"; // Import the Book model
 
 const app = express();
 
-//connect to db
+// Connect to the database
 mongoose
   .connect(DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
+    // Start the server after successfully connecting to the database
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
@@ -20,46 +21,48 @@ mongoose
 
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error: "));
+// Database connection error handling
+db.on("error", console.error.bind(console, "Connection error: "));
 db.once("open", () => {
   console.log("Database connected");
 });
-//middleware
-app.use(cors());
-app.use(express.json());
 
-//get
+// Middleware
+app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
+app.use(express.json()); // Parse incoming JSON data
+
+// Route to the root of the application
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// get all books
+// Get all books
 app.get("/books", async (req, res) => {
   try {
-    const books = await Book.find({});
-    res.status(200).send(books);
+    const books = await Book.find({}); // Find all books in the database
+    res.status(200).send(books); // Respond with a status of 200 (OK) and send the list of books
   } catch (error) {
     console.log(error);
   }
 });
 
-// get a book by id
+// Get a book by its ID
 app.get("/books/:id", async (req, res) => {
   try {
-    const book = await Book.findById(req.params.id);
-    res.status(200).send(book);
+    const book = await Book.findById(req.params.id); // Find a book by its ID
+    res.status(200).send(book); // Respond with a status of 200 (OK) and send the book data
   } catch (error) {
     console.log(error);
   }
 });
 
-//update a book by id
+// Update a book by its ID
 app.put("/books/:id", async (req, res) => {
   try {
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
       res.status(400).send("Please fill all the fields");
     }
-    const result = Book.findByIdAndUpdate(req.params.id, req.body);
+    const result = await Book.findByIdAndUpdate(req.params.id, req.body); // Update the book by its ID
     if (!result) {
       res.status(400).send("Book not found");
     }
@@ -69,7 +72,7 @@ app.put("/books/:id", async (req, res) => {
   }
 });
 
-//post
+// Create a new book
 app.post("/books", async (req, res) => {
   try {
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
@@ -80,23 +83,22 @@ app.post("/books", async (req, res) => {
       author: req.body.author,
       publishYear: req.body.publishYear,
     });
-    const book = await Book.create(newBook);
-    res.status(201).send(book);
+    const book = await Book.create(newBook); // Create a new book in the database
+    res.status(201).send(book); // Respond with a status of 201 (Created) and send the created book
   } catch (error) {
     console.log(error);
   }
 });
 
-
-//delete
+// Delete a book by its ID
 app.delete("/books/:id", async (req, res) => {
-    try {
-        const result = await Book.findByIdAndDelete(req.params.id);
-        if (!result) {
-            res.status(400).send("Book not found");
-        }
-        res.status(200).send("Book deleted successfully");
-    } catch (error) {
-        console.log(error);
+  try {
+    const result = await Book.findByIdAndDelete(req.params.id); // Find and delete a book by its ID
+    if (!result) {
+      res.status(400).send("Book not found");
     }
+    res.status(200).send("Book deleted successfully");
+  } catch (error) {
+    console.log(error);
+  }
 });
